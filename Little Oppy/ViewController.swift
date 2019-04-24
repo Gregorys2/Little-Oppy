b0072626844fef8fe9f2e194c91226c6f67bd39d
Your Name <you@example.com>
2019-04-24 14:30:26 -0600

    Initial Commit

---
+++ Little Oppy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Little Oppy
+//
+//  Created by Gregory Paul Sim on 2019-04-24.
+//  Copyright © 2019 Gregory Paul Sim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
